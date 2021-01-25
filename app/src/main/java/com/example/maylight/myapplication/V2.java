6433c6fbf7aea7ec333c546b8fa4bee505b5a469
v_vggmei <guang.mei@nx-engine.com>
2021-01-25 11:26:09 +0800

    提交master_meiguang0125_1125

--- app/src/main/java/com/example/maylight/myapplication/V2.java
+++ app/src/main/java/com/example/maylight/myapplication/V2.java
@@ -10,5 +10,6 @@ public class V2 {
 		System.out.println("新写的代码");
 		System.out.println("2021年1月23日16:54");
 		System.out.println("bugfix/master_meiguang0123_1716");
+		System.out.println("bugfix/master_meiguang0125_1125");
 	}
 }
