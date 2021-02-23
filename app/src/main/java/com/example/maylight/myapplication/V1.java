0165bf48fe07774b3becfc25ef68a82c6ef5d444
maylight <1667480594@qqq.com>
2021-02-23 21:57:38 +0800

    提交bugfix/master_meiguang0223_2156_shenzhou

--- app/src/main/java/com/example/maylight/myapplication/V1.java
+++ app/src/main/java/com/example/maylight/myapplication/V1.java
@@ -8,5 +8,6 @@ public class V1 {
 	public static void main(String[] args) {
 		System.out.println("在master的V1.java上写的代码");
 		System.out.println("创建无关紧要的东西代码");
+		System.out.println("bugfix/master_meiguang0223_2156_shenzhou");
 	}
 }
